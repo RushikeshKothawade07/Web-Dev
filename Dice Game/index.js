19bd143fc5db36738cc81eb860bd7925bfabebcf
Rushikesh Kothawade <97045515+RushikeshKothawade07@users.noreply.github.com>
2022-10-27 17:27:48 +0530

    Add files via upload
    Dice Game completed using basic js

---
+++ Dice Game/index.js
@@ -0,0 +1,18 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomImage1="images/dice"+randomNumber1+".png";
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImage1);
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomImage2="images/dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomImage2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 Won! 🚩"
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 Won! 🚩"
+}
+else{
+    document.querySelector("h1").innerHTML="Oooo..Draw!"
+}
